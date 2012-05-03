72c527ced78e86c6fa48a5a34ea39b441857075d
Dan Mane <danmane@gmail.com>
2012-05-03 01:58:39 -0500

    Working parser

--- data_manager.py
+++ data_manager.py
@@ -1,39 +1,64 @@
-
+#!/usr/bin/env python
 '''
 This module manages and maintains data for RoboBuffett
 '''
 import string
 import os
+import sys
+from datetime import date, timedelta
+import logging
 from pdb import set_trace as debug
+try:
+	import cPickle as pickle
+except:
+	import pickle
 
-# class financial_universe:
-# 	'''Manage all financial information for all of the stocks. This will be the object that we pickle for serialization. For memory efficiency, it will not contain the full raw text of documents, but will contain methods by which they can be loaded into memeory.
-# 	Perhaps we will want to include the word frequency counts since they will require less space. Will have to see whether it's practical.
-# 	'''
-# 	def __init__(self):
-# 		self.companies  = {}
-# 		self.industries = {}
-# 		for document in our_SEC_Documents:
-# 			company_quarter = make_company_quarter(document)
-# 			if company_quarter.companyID not in self.companies:
-# 				self.companies[companyID] = make_company(document) # Create a new company entry based on the document
-# 			else:
-# 				self.companies[companyID].add_quarter
-
-# 			if document.industryID not in self.industries:
-# 				self.industries[industryID] = industry(document) #Create a new industry entry
-# 			else
-# 				self.industries[industryID].add_new_document()
-
-
-# 	def add_new_company(self, company):
+class financial_universe:
+	'''Manage all financial information for all of the stocks. This will be the object that we pickle for serialization. For memory efficiency, it will not contain the full raw text of documents, but will contain methods by which they can be loaded into memeory.
+	Perhaps we will want to include the word frequency counts since they will require less space. Will have to see whether it's practical.
+	'''
+	def __init__(self, data_dir):
+		self.companies  = {}
+		self.industries = {}
+		self.documents  = []
+		for doc_path in os.listdir(data_dir):
+			if doc_path[0] == ".": continue
+			newdoc = document(data_dir + doc_path, 'SEC_Quarterly')
+			self.documents.append(newdoc)
+			CIK = newdoc.CIK
+			if CIK not in self.companies:
+				self.companies[CIK] = company(newdoc) # Create a new company entry based on the document
+			else:
+				self.companies[CIK].add_document(newdoc)
+		self.get_counts()
 
 
-# class company:
-# 	def __init__(self):
-# 		company.industry = #Parse the document to get a SIC industry code
-# 		company.timerange = #(startQuarter, endQuarter) showing when data begins and when data ends. e.g. (2002.75, 2012.25)
-# 		company.quarters = {2002.25: company_quarter, ...}#dict of company_quarter objects indexed by the quarter
+	def get_counts(self):
+		sum = 0
+		for doc in self.documents:
+			sum += doc.word_count
+		self.word_count = sum
+		self.doc_count = len(self.documents)
+		self.co_count = len(self.companies)
+		self.indy_count = len(self.industries)
+
+
+
+class company:
+	def __init__(self, document):
+		company.CIK = document.CIK
+		company.SIC = document.SIC
+		company.documents = [(document.date, document)]
+		company.name = document.cname
+
+	def add_document(self, document):
+		company.documents.append((document.date, document))
+		if document.cname != company.name:
+			print "Name discrepancy: %s, %s" % (company.name, document.cname)
+			logging.debug("Name discrepancy: %s, %s" % (company.name, document.cname))
+		if document.SIC != company.SIC:
+			print "SIC discrepancy: %d %d" % (company.SIC, document.SIC)
+			logging.debug("SIC discrepancy: %d %d" % (company.SIC, document.SIC))
 
 # class industry:
 # 	def __init__(self):
@@ -52,23 +77,24 @@ class document:
 	def __init__(self, docpath, doctype):
 		'''Populate the following'''
 		self.path = docpath
-		self.type = doctype
 		self.properties = {}
 		self.word_freq = {}
 		self.word_count = {}
 		try:
-			self.file = open(docpath, 'r')
+			self.docfile = open(docpath, 'r')
 		except IOError:
 			print "Bad file path ", docpath
-			return 02
+			logging.warning('Bad doc path: %s' % docpath)
+			return 
 
 
-		if self.type in ('10-Q', '10-K'):
+		if doctype == 'SEC_Quarterly':
 			self.parse_quarterly_filing()
 		else:
 			print "Document not supported: %s type %s" % (docpath, doctype)
+			logging.warning('Unsupported doc %s type %s' % (docpath, doctype))
 
-		self.file.close()
+		self.docfile.close()
 
 
 	def parse_quarterly_filing(self):
@@ -76,8 +102,8 @@ class document:
 		property_info = ( 
 		  #DictionaryName,    FilingText,                          isNumber
 		 ('DocType',         'CONFORMED SUBMISSION TYPE:',          False),
-		 ('ReportingPeriod', 'CONFORMED PERIOD OF REPORT:',         True ),
-		 ('FilingDate',      'FILED AS OF DATE:',                   True ),
+		 ('ReportingPeriod', 'CONFORMED PERIOD OF REPORT:',         False),
+		 ('FilingDate',      'FILED AS OF DATE:',                   False),
 	 	 ('CompanyName',     'COMPANY CONFORMED NAME:',             False),
 		 ('CIK',             'CENTRAL INDEX KEY:',                  True ),
 		 ('SIC',             'STANDARD INDUSTRIAL CLASSIFICATION:', True ),
@@ -87,10 +113,13 @@ class document:
 		# Defines the properties to seek in the header of the filing, and names to assign them to in the self.properties dictionary. I hope Python doesn't waste time re-creating this tuple every time parse_quarterly_filing is called.
 		# The last condition 'Item 1. B' triggers when we have parsed all the header info and are into the actual document. Since the dictionaryName is '' it won't store anything, but it returns a nonzero value so that the loop will break
 
-		text = self.file.read().partition('ITEM 1. BUSINESS')
-		if text[2] == '':
-			print "ERROR: Bad partition %s %s" % (docpath, doctype)
-			return 0
+		partition_text = 'PART I'
+		text = self.docfile.read().partition(partition_text)
+		# Currently I partition it into Header and Body by seperating at the first instance of the text 'PART I'. I consider this a placeholder
+		if text[1] != partition_text:
+			print "Warning: Unable to partition %s" % (self.path)
+			logging.warning("ERROR: Unable to partition %s" % (self.path))
+			return
 		header = text[0].split("\n") #Consider mapping .strip for efficiency
 		text = text[2]
 
@@ -102,14 +131,19 @@ class document:
 				i += 1
 				if i == len(property_info):
 					break
-		print "Found %d of %d fields" % (i, len(property_info))
+		if i != len(property_info):
+			print "Found %d of %d fields for %s" % (i, len(property_info), self.path)
+			logging.warning("Found %d of %d fields for %s" % (i, len(property_info), self.path))
+		self.convert_property_to_date('ReportingPeriod')
+		self.convert_property_to_date('FilingDate')
 
-		if self.properties[DocType] != self.type:
-			print "ERROR: Conflicting types for %s: %s reported %s discovered." % (self.path, self.type, self.properties[DocType])
-		del self.properties[DocType]
+		self.date  = self.properties['FilingDate']
+		self.type  = self.properties['DocType']
+		self.CIK   = self.properties['CIK']
+		self.SIC   = self.properties['SIC']
+		self.cname = self.properties['CompanyName']
 
-		'Now grab the entire document and process it to make a wordfreq'
-		debug()
+		'Now grab the entire document and process it to make a word_freq'
 		text = text.translate(None, (string.punctuation + string.digits))
 		text = text.lower().split()
 		# Reads all words left in the file, removes all punctuation or numbers, converts to lowercase
@@ -129,7 +163,35 @@ class document:
 				try:
 					content = int(content)
 				except ValueError:
-					debug()
+					logging.debug('''ValueError occured converting "%s" to int in line:\n%s . Forcing conversion.''' % (content, line))
+					try: 
+						content = int(content.translate(None, string.punctuation + string.ascii_letters + string.whitespace))
+					except ValueError:
+						logging.error('Unable to force-convert line to get int value.')
 			if propertyname != '':
 				self.properties[propertyname] = content
 			return content
+	
+	def convert_property_to_date(self, propertyname):
+		prop = self.properties[propertyname]
+		yyyy = int(prop[0:4])
+		mm   = int(prop[4:6])
+		dd   = int(prop[6:8])
+		self.properties[propertyname] = date(yyyy, mm, dd)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		data_dir = "./Data/"
+	else:
+		data_dir = argv[1]
+
+	logging.basicConfig(filename='data_manager.log', level=logging.DEBUG)
+	universe = financial_universe(data_dir)
+
+	print "Statistics: %d documents, %d companies %d words" % (universe.doc_count, universe.co_count, universe.word_count)
+	logging.info("Statistics: %d documents, %d companies %d words" % (universe.doc_count, universe.co_count, universe.word_count))
+	f = open('./universe.dat', 'w')
+	pickle.dump(universe, f, 2)
+	f.close()
+
