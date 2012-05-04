778367ca43b8ba9408eddbcd721f5e331bdf6ae2
Dan Mane <danmane@gmail.com>
2012-05-03 22:45:50 -0500

    Stock prices from Ahmad, and improvements to data manager

--- data_manager.py
+++ data_manager.py
@@ -13,7 +13,7 @@ try:
 except:
 	import pickle
 
-class financial_universe:
+class Financial_Universe:
 	'''Manage all financial information for all of the stocks. This will be the object that we pickle for serialization. For memory efficiency, it will not contain the full raw text of documents, but will contain methods by which they can be loaded into memeory.
 	Perhaps we will want to include the word frequency counts since they will require less space. Will have to see whether it's practical.
 	'''
@@ -23,13 +23,19 @@ class financial_universe:
 		self.documents  = []
 		for doc_path in os.listdir(data_dir):
 			if doc_path[0] == ".": continue
-			newdoc = document(data_dir + doc_path, 'SEC_Quarterly')
+			newdoc = Document(data_dir + doc_path, 'SEC_Quarterly')
 			self.documents.append(newdoc)
 			CIK = newdoc.CIK
 			if CIK not in self.companies:
-				self.companies[CIK] = company(newdoc) # Create a new company entry based on the document
+				self.companies[CIK] = Company(newdoc) # Create a new company entry based on the document
 			else:
 				self.companies[CIK].add_document(newdoc)
+
+		for CIK, company in self.companies.iteritems():
+			if CIK not in self.industries:
+				self.industries[CIK] = Industry(CIK, company)
+			else:
+				self.industries[CIK].add_company(CIK, company)
 		self.get_counts()
 
 
@@ -43,16 +49,15 @@ class financial_universe:
 		self.indy_count = len(self.industries)
 
 
-
-class company:
+class Company:
 	def __init__(self, document):
-		company.CIK = document.CIK
-		company.SIC = document.SIC
-		company.documents = [(document.date, document)]
-		company.name = document.cname
+		self.CIK = document.CIK
+		self.SIC = document.SIC
+		self.documents = [(document.date, document)]
+		self.name = document.cname
 
 	def add_document(self, document):
-		company.documents.append((document.date, document))
+		self.documents.append((document.date, document))
 		if document.cname != company.name:
 			print "Name discrepancy: %s, %s" % (company.name, document.cname)
 			logging.debug("Name discrepancy: %s, %s" % (company.name, document.cname))
@@ -60,11 +65,18 @@ class company:
 			print "SIC discrepancy: %d %d" % (company.SIC, document.SIC)
 			logging.debug("SIC discrepancy: %d %d" % (company.SIC, document.SIC))
 
-# class industry:
-# 	def __init__(self):
-# 		industry.components = 
+class Industry:
+	def __init__(self, CIK, company):
+		self.SIC = company.SIC
+		self.components = {CIK: company}
+		self.n_componenets = 1
+	
+	def add_company(self, CIK, company):
+		if CIK not in self.components:
+			self.components[CIK] = company
+			self.n_components += 1
 
-# class quarter:
+# class Quarter:
 # 	def __init__(self, document):
 # 		self.companyID = company.ID
 # 		self.quarterID   = quarter #e.g. 2002.25
@@ -73,7 +85,7 @@ class company:
 # 		self.opening_price = get_opening_price(company, filingdate)
 #  # Pull information from the document to populate the 
 
-class document:
+class Document:
 	def __init__(self, docpath, doctype):
 		'''Populate the following'''
 		self.path = docpath
@@ -95,6 +107,7 @@ class document:
 			logging.warning('Unsupported doc %s type %s' % (docpath, doctype))
 
 		self.docfile.close()
+		del self.docfile
 
 
 	def parse_quarterly_filing(self):
@@ -166,8 +179,8 @@ class document:
 					logging.debug('''ValueError occured converting "%s" to int in line:\n%s . Forcing conversion.''' % (content, line))
 					try: 
 						content = int(content.translate(None, string.punctuation + string.ascii_letters + string.whitespace))
-					except ValueError:
-						logging.error('Unable to force-convert line to get int value.')
+					except ValueError as e:
+						logging.error('Unable to force-convert ' + str(e))
 			if propertyname != '':
 				self.properties[propertyname] = content
 			return content
@@ -186,12 +199,14 @@ if __name__ == "__main__":
 	else:
 		data_dir = argv[1]
 
-	logging.basicConfig(filename='data_manager.log', level=logging.DEBUG)
-	universe = financial_universe(data_dir)
+	with open('./data_manager.log', 'w') as cleanlog:
+		pass
 
-	print "Statistics: %d documents, %d companies %d words" % (universe.doc_count, universe.co_count, universe.word_count)
-	logging.info("Statistics: %d documents, %d companies %d words" % (universe.doc_count, universe.co_count, universe.word_count))
-	f = open('./universe.dat', 'w')
-	pickle.dump(universe, f, 2)
-	f.close()
+	logging.basicConfig(filename='data_manager.log', level=logging.DEBUG)
+	universe = Financial_Universe(data_dir)
+
+	print "Statistics: %d documents, %d companies %d industries %d words" % (universe.doc_count, universe.co_count, universe.indy_count, universe.word_count)
+	logging.info("Statistics: %d documents, %d companies %d industries %d words" % (universe.doc_count, universe.co_count, universe.indy_count, universe.word_count))
+	with open('./universe.dat', 'w') as f:
+		pickle.dump(universe, f, 2)
 
